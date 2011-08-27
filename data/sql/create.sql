abf8b81afdddc50ef6ed26245a9842c247654b5b
Lea Haensenberger <lea.haensenberger@gmail.com>
2011-08-27 13:05:14 +0200

    Adding doctrine extension and creation sql scripts

---
+++ data/sql/create.sql
@@ -0,0 +1,26 @@
+--
+-- Structure for table `film`
+--
+
+CREATE TABLE `film` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name_1` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name_2` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Structure for table `vote`
+--
+
+CREATE TABLE `vote` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `film_id` int(10) unsigned NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `ip` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `film_id` (`film_id`),
+  CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`film_id`) REFERENCES `film` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- index.php
+++ index.php
@@ -2,12 +2,43 @@
 
 require_once __DIR__.'/vendor/silex.phar';
 
+$env = 'dev';
+if (preg_match('/fly-and\.ch/', $_SERVER['HTTP_HOST'])){
+    $env = 'prod';
+}
+
 $app = new Silex\Application();
+$app['debug'] = $env == 'dev';
 
 // twig
 $app->register(new Silex\Extension\TwigExtension(), array(
     'twig.path'       => __DIR__.'/views',
     'twig.class_path' => __DIR__.'/vendor/twig/lib',
+));
+
+// doctrine
+if ($env == 'prod') {
+    $dbOptions = array(
+        'driver' => 'pdo_mysqli',
+        'host' => '',
+        'user' => '',
+        'password' => '',
+        'dbname' => 'fly_and_film',
+    );
+} else {
+    $dbOptions = array(
+        'driver' => 'pdo_mysqli',
+        'host' => 'localhost',
+        'user' => 'fly_and',
+        'password' => 'cn3WExfHS5zReTQR',
+        'dbname' => 'fly_and_film',
+    );
+}
+
+$app->register(new Silex\Extension\DoctrineExtension(), array(
+    'db.options'            => $dbOptions,
+    'db.dbal.class_path'    => __DIR__.'/vendor/doctrine-dbal/lib',
+    'db.common.class_path'  => __DIR__.'/vendor/doctrine-common/lib',
 ));
 
 // home
